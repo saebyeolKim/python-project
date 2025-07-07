0674ab96e45bd1cf8465da93b84472acd571a693
saebyeolKim <toquf2611@naver.com>
2025-07-07 22:48:13 +0900

    feat: UserRepository 구현

--- app/user/infra/repository/user_repo.py
+++ app/user/infra/repository/user_repo.py
@@ -1,4 +1,50 @@
-from app.user.domain.repository.user_repo import IUserRepository
+from database import SessionLocal
+from user.domain.repository.user_repo import IUserRepository
+from user.domain.user import User as UserVO, Profile as ProfileVO # 모델
+from user.infra.db_models.user import User # 엔티티
+from fastapi import HTTPException
+from utils.db_utils import row_to_dict
 
 class UserRepository(IUserRepository):
-    pass
+    def save(self, user: UserVO):
+        new_user = User(
+            id=user.id,
+            email=user.profile.email,
+            name=user.profile.name,
+            password=user.password,
+            created_at=user.created_at,
+            updated_at=user.updated_at,
+        )
+    
+        with SessionLocal() as db:
+            try:
+                db = SessionLocal()
+                db.add(new_user)
+                db.commit()
+            finally:
+                db.close()
+
+    def find_by_email(self, email: str) -> UserVO:
+        with SessionLocal() as db:
+            try:
+                user = db.query(User).filter(User.email == email).first()
+            finally:
+                db.close()
+            
+        if not user:
+            raise HTTPException(status_code=422)
+            
+        # return UserVO(
+        #     id=user.id,
+        #     profile=ProfileVO(
+        #         name=user.name,
+        #         email=user.email
+        #     ),
+        #     password=user.password,
+        #     created_at=user.created_at,
+        #     updated_at=user.updated_at
+        # )
+    
+        return UserVO(row_to_dict(user))
+
+
